d039ad35211d5597352b71c05e0d5de92075e483
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-11-05 12:59:22 +0530

    updated news api

--- news.py
+++ news.py
@@ -1,6 +1,6 @@
 import requests
 def getNews():
-    url = "https://inshorts.me/news/all?offset=0&limit=10"
+    url = "https://inshorts.vercel.app/news/all?offset=0&limit=10"
     response = requests.get(url)
     newsdata = response.json()["data"]["articles"]
     return newsdata
